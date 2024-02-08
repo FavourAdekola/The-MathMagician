d19c750e3a4f6a229f933ce0ec08678aa977b20b
FavourAdekola <favouradekola22@gmail.com>
2024-02-07 22:21:11 -0500

    Maze Level Tweak

--- Scripts/city_maze.gd
+++ Scripts/city_maze.gd
@@ -29,4 +29,4 @@ func _on_fall_pit_body_exit(body):
 
 func _on_exit_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene_to_file("res://Scenes/City_Maze.tscn")
+		get_tree().change_scene_to_file("res://Scenes/king_castle.tscn")
